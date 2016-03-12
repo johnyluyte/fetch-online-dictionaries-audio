062cdaf663677cd5d0e885bb62033092d8e9413d
Chien Chun <johnyluyte@gmail.com>
2016-03-12 16:48:10 +0800

    fix bad delimiter in message.json

--- options.js
+++ options.js
@@ -1,17 +1,23 @@
+// Show upported site title
+var title = "<p>" + chrome.i18n.getMessage("textSupportedSites") + "</p>";
+if(!$("#div_site_list_title").length==0){
+    $("#div_site_list_title").append(title);
+}
+
 // Show upported site lists
 var lists = chrome.i18n.getMessage("textSupportedSitesLists").split(",");
-var show = "<p>" + chrome.i18n.getMessage("textSupportedSites") + "</p>";
-show += "<ul>";
+var show = "<ul>";
 var len = lists.length;
 for(var i=0; i<len; i++){
     if(lists[i]==""){continue;};
     show += "<li><a target='_blank' href='" + lists[i] + "'>" + lists[i] + "</a></li>";
 }
 show += "</ul>";
-show += "<b>" + chrome.i18n.getMessage("textCopyRight") + "</b><br/>";
-show += "<hr>";
 $("#div_site_lists").append(show);
 
+// Copyright
+var copyright = "<b>" + chrome.i18n.getMessage("textCopyRight") + "</b><br/>";
+$("#div_copyright").append(copyright);
 
 // Suggestions, issues
 var issues = "<p>" + chrome.i18n.getMessage("textIssues");
