951464189cf421aee77d1f53ed53f24a05df6928
ChunNorris <johnyluyte@gmail.com>
2020-02-23 14:29:05 +0800

    check OnInstalledReason before showing updates.html

--- background.js
+++ background.js
@@ -4,10 +4,9 @@
 */
 function checkForValidUrl(tabId, changeInfo, tab) {
   chrome.pageAction.show(tabId);
-};
+}
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
-
 
 /*
   `content_script.js` and `background.js` use "Message Passing" to communicate with each other.
@@ -18,21 +17,29 @@ chrome.tabs.onUpdated.addListener(checkForValidUrl);
     https://developer.chrome.com/extensions/tabs
     https://developer.chrome.com/extensions/messaging
 */
-chrome.pageAction.onClicked.addListener(function(tab){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {greeting: "onClickedPageActionButton"}, function(response) {
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { greeting: "onClickedPageActionButton" },
+      function(response) {
         // do nothing
-      });
-    });
+      }
+    );
+  });
 });
-
 
 /*
   Fired when the extension is first installed, when the extension is updated to a new version, and when Chrome is updated to a new version.
   Reference:
-    https://developer.chrome.com/extensions/runtime#event-onInstalled
+    https://developer.chrome.com/extensions/runtime
 */
-chrome.runtime.onInstalled.addListener(function(){
-  console.log("oninstall");
-  chrome.tabs.create({ url: "updates.html" });
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (
+    details.reason === chrome.runtime.OnInstalledReason.INSTALL ||
+    details.reason === chrome.runtime.OnInstalledReason.UPDATE
+  ) {
+    console.log("oninstalled or updated");
+    chrome.tabs.create({ url: "updates.html" });
+  }
 });
